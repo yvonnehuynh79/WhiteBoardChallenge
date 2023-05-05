41659da057facd2c90b0b203bb96481e288b6727
yvonnehuynh79 <yvonnehuynh888@gmail.com>
2023-05-05 08:52:25 -0700

    second commit

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+
+
+
+static string FizzBuzz(int number)
+{
+    if (number % 3 == 0 && number % 5 == 0)
+    {
+        return "fizzbuzz";
+    }
+
+    if (number % 3 == 0)
+    {
+        return "fizz";
+    }
+    if (number % 5 == 0)
+    {
+        return "buzz";
+    }
+    
+    return $"Try again! this number is not divisible by 3 nor 5!";
+}
+var answer = FizzBuzz(8);
+Console.WriteLine(answer);
+
