0ce6c16820eabc2783ac1ce0347501156a274ba9
yvonnehuynh79 <yvonnehuynh888@gmail.com>
2023-05-05 08:31:40 -0700

    initial commit

---
+++ FizzBuzzWhiteBoardChallenge.cs
@@ -0,0 +1,13 @@
+public string FizzBuzz(number)
+if(number % 3 == 0)
+{
+    return "fizz";
+}
+if(number % 5 ==0)
+{
+return "buzz";
+}
+if(number % 3 == 0 && number % 5 == 0)
+{
+    return "fizzbuzz";
+}
